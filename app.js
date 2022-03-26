8289e2a437ebe585cba9284f7e99f8dd124a81a4
MantillaBigfoot <lotz.christoph@outlook.com>
2022-03-26 11:16:17 +0100

    add sites

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser());
+
+// __dirname = abs path to current file
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/german', (request, response) => {});
+
+app.get('/german', (request, response) => {
+  response.sendFile(__dirname + '/views/german.html');
+});
+
+app.get('/english', (request, response) => {
+  response.sendFile(__dirname + '/views/english.html');
+});
+
+app.get('/spanish', (request, response) => {
+  response.sendFile(__dirname + '/views/spanish.html');
+});
+
+//look inside the public dir
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+app.get('*', (request, response) => {
+  response.send('Oh, there was a problem....site not reachable');
+});
+
+app.listen(3000);
